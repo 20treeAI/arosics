6006cf4b4314e0dec782fd41eacfbb553c5cbae3
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-03-27 12:27:57 +0100

    perf: scipy.fft

--- arosics/CoReg.py
+++ arosics/CoReg.py
@@ -33,6 +33,7 @@ from multiprocessing import cpu_count
 # custom
 from osgeo import gdal  # noqa
 import numpy as np
+import scipy
 
 from packaging.version import parse as parse_version
 try:
@@ -1186,8 +1187,8 @@ class COREG(object):
                     in_arr1 = im1[ymin:ymax, xmin:xmax].astype(precision)
 
             if self.fftw_works is False or fft_arr0 is None or fft_arr1 is None:
-                fft_arr0 = np.fft.fft2(in_arr0)
-                fft_arr1 = np.fft.fft2(in_arr1)
+                fft_arr0 = scipy.fft.fft2(in_arr0)
+                fft_arr1 = scipy.fft.fft2(in_arr1)
 
             # GeoArray(fft_arr0.astype(np.float32)).show(figsize=(15,15))
             # GeoArray(fft_arr1.astype(np.float32)).show(figsize=(15,15))
@@ -1205,13 +1206,13 @@ class COREG(object):
             if 'pyfft' in globals():
                 ifft_arr = pyfftw.FFTW(temp, np.empty_like(temp), axes=(0, 1), direction='FFTW_BACKWARD')()
             else:
-                ifft_arr = np.fft.ifft2(temp)
+                ifft_arr = scipy.fft.ifft2(temp)
             if self.v:
                 print('backward FFTW: %.2fs' % (time.time() - time0))
 
             cps = np.abs(ifft_arr)
             # scps = shifted cps  => shift the zero-frequency component to the center of the spectrum
-            scps = np.fft.fftshift(cps)
+            scps = scipy.fft.fftshift(cps)
             if self.v:
                 PLT.subplot_imshow([np.real(in_arr0).astype(np.uint16), np.real(in_arr1).astype(np.uint16),
                                     np.real(fft_arr0).astype(np.uint8), np.real(fft_arr1).astype(np.uint8), scps],
